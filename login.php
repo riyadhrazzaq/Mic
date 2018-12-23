3f3e13ff9b99af842f959992f0d1e0602e3451db
riyadhrazzaq <riyadh.razzaq@gmail.com>
2018-12-24 04:37:53 +0600

    login done

--- login.php
+++ login.php
@@ -9,11 +9,41 @@
     <meta content="" name="author" />
     <link rel="stylesheet" type="text/css" href="assets/plugins/owl-carousel/owl.carousel.css" />
     <link rel="stylesheet" type="text/css" href="assets/plugins/owl-carousel/owl.theme.css" />
-    <link rel="stylesheet" type="text/css" href="assets/plugins/onescroll/css/demo.css" />
-    <link rel="stylesheet" type="text/css" href="assets/plugins/onescroll/css/component.css" />
     <link rel="stylesheet" type="text/css" href="assets/plugins/headereffects/css/component.css">
     <link rel="stylesheet" type="text/css" href="assets/plugins/headereffects/css/normalize.css" />
-    <link rel="stylesheet" type="text/css" href="assets/plugins/headereffects/css/login.css" />
+    <link href="assets/plugins/pace/pace-theme-flash.css" rel="stylesheet" type="text/css"
+        media="screen" />
+    <!-- BEGIN CORE CSS FRAMEWORK -->
+    <link href="assets/plugins/boostrapv3/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/plugins/boostrapv3/css/bootstrap-theme.min.css" rel="stylesheet"
+        type="text/css" />
+    <link href="assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet"
+        type="text/css" />
+    <!-- END CORE CSS FRAMEWORK -->
+    <!-- BEGIN CSS TEMPLATE -->
+    <link href="assets/css/style.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/magic_space.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/responsive.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/animate.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/main_login.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/util.css" rel="stylesheet" type="text/css" />
+    <!-- END CSS TEMPLATE -->
+</head>
+<!-- END HEAD -->
+<body>
+    <!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+    <meta charset="utf-8" />
+    <title>thempleite - Responsive Website Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta content="" name="description" />
+    <meta content="" name="author" />
+    <link rel="stylesheet" type="text/css" href="assets/plugins/owl-carousel/owl.carousel.css" />
+    <link rel="stylesheet" type="text/css" href="assets/plugins/owl-carousel/owl.theme.css" />
+    <link rel="stylesheet" type="text/css" href="assets/plugins/headereffects/css/component.css">
+    <link rel="stylesheet" type="text/css" href="assets/plugins/headereffects/css/normalize.css" />
     <link href="assets/plugins/pace/pace-theme-flash.css" rel="stylesheet" type="text/css"
         media="screen" />
     <!-- BEGIN CORE CSS FRAMEWORK -->
@@ -29,29 +59,96 @@
     <link href="assets/css/responsive.css" rel="stylesheet" type="text/css" />
     <link href="assets/css/animate.min.css" rel="stylesheet" type="text/css" />
     <!-- END CSS TEMPLATE -->
+    <script type="text/javascript" src="assets/plugins/jquery-1.8.3.min.js"></script>
+    <!-- SLIDER REVOLUTION 4.x SCRIPTS  -->
+    <script type="text/javascript" src="assets/plugins/slider-plugin/js/slider1.min.js"></script>
+    <script type="text/javascript" src="assets/plugins/slider-plugin/js/slider2.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="assets/plugins/slider-plugin/css/settings.css"
+        media="screen" />
 </head>
 <!-- END HEAD -->
 <body>
+<div role="navigation" class="navbar navbar-default navbar-static-top">
+            <div class="container">
+                <div class="compressed">
+                    <div class="navbar-header">
+                        <button data-target=".navbar-collapse" data-toggle="collapse" class="navbar-toggle" type="button">
+                            <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+                        </button>
+                        <a href="#" class="navbar-brand compressed">
+                        <img src="assets/img/logo_mic_sized.png" alt="" data-src="assets/img/logo_mic_sized.png" data-src-retina="assets/img/logo2x.png" width="97" height="50"></a>
+                    </div>
+                    <div class="navbar-collapse collapse">
+                            <ul class="nav navbar-nav navbar-right">
+                                <li><a href="index.php">Home</a></li>
+                                <li><a href="tour.php">Tour</a></li>
+                                <li><a href="portfolio.php">Portfolio</a></li>
+                                <li><a href="contact.php">Contact</a></li>
+                                <li><a href="login.php">Login</a></li>
+                            </ul>
+                        </div>
+                    <!--/.nav-collapse -->
+                </div>
+            </div>
+    </div>
+    <div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<form class="login100-form validate-form">
+					<span class="login100-form-title p-b-26">
+						Welcome
+					</span>
+					<span class="login100-form-title p-b-48">
+						<i class="zmdi zmdi-font"></i>
+					</span>
 
-</body>
-    <!-- BEGIN CORE JS FRAMEWORK -->
-    <script type="text/javascript" src="assets/plugins/jquery-1.8.3.min.js"></script>
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is: a@b.c">
+						<input class="input100" type="text" name="email">
+						<span class="focus-input100" data-placeholder="Email"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate="Enter password">
+						<span class="btn-show-pass">
+							<i class="zmdi zmdi-eye"></i>
+						</span>
+						<input class="input100" type="password" name="pass">
+						<span class="focus-input100" data-placeholder="Password"></span>
+					</div>
+
+					<div class="container-login100-form-btn">
+						<div class="wrap-login100-form-btn">
+							<div class="login100-form-bgbtn"></div>
+							<button class="login100-form-btn">
+								Login
+							</button>
+						</div>
+					</div>
+
+					<div class="text-center p-t-115">
+						<span class="txt1">
+							Don’t have an account?
+						</span>
+
+						<a class="txt2" href="#">
+							Sign Up
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+    <script src="assets/js/main.js"></script>
     <script src="assets/plugins/boostrapv3/js/bootstrap.min.js" type="text/javascript"></script>
-    <!-- END CORE JS FRAMEWORK -->
-    <!-- BEGIN JS PLUGIN -->
     <script src="assets/plugins/pace/pace.min.js" type="text/javascript"></script>
-    <script src="assets/plugins/jquery-unveil/jquery.unveil.min.js" type="text/javascript"></script>
     <script src="assets/plugins/owl-carousel/owl.carousel.min.js" type="text/javascript"></script>
-    <script src="assets/plugins/modernizr.custom.js"></script>
     <script src="assets/plugins/waypoints.min.js"></script>
-    <script type="text/javascript" src="assets/plugins/parrallax/js/jquery.parallax-1.1.3.js"></script>
-    <script src="assets/plugins/onescroll/js/classie.js"></script>
-    <script src="assets/plugins/onescroll/js/cbpScroller.js"></script>
-    <script type="text/javascript" src="assets/plugins/jquery-nicescroll/jquery.nicescroll.min.js"></script>
-    <!-- END JS PLUGIN -->
-    <script>
-        new cbpScroller(document.getElementById('cbp-so-scroller'));
-    </script>
     <script type="text/javascript" src="assets/js/core.js"></script>
+
+    <!--===============================================================================================-->
+	<script type="text/javascript" src="assets/plugins/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="assets/plugins/animsition.min.js"></script>
+	<script type="text/javascript" src="assets/js/main.js"></script>
+    <!--===============================================================================================-->
+</body>
 </body>
 </html>
