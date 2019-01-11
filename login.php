f31ab3288c6ab3c35b55a277ae2ad9f97c924b7d
riyadhrazzaq <riyadh.razzaq@gmail.com>
2019-01-12 04:18:34 +0600

    signup,login,sessionmanager

--- login.php
+++ login.php
@@ -31,43 +31,108 @@
 </head>
 <!-- END HEAD -->
 <body>
-    <!DOCTYPE html>
-<html>
-<head>
-    <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-    <meta charset="utf-8" />
-    <title>thempleite - Responsive Website Template</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-    <meta content="" name="description" />
-    <meta content="" name="author" />
-    <link rel="stylesheet" type="text/css" href="assets/plugins/owl-carousel/owl.carousel.css" />
-    <link rel="stylesheet" type="text/css" href="assets/plugins/owl-carousel/owl.theme.css" />
-    <link rel="stylesheet" type="text/css" href="assets/plugins/headereffects/css/component.css">
-    <link rel="stylesheet" type="text/css" href="assets/plugins/headereffects/css/normalize.css" />
-    <link href="assets/plugins/pace/pace-theme-flash.css" rel="stylesheet" type="text/css"
-        media="screen" />
-    <!-- BEGIN CORE CSS FRAMEWORK -->
-    <link href="assets/plugins/boostrapv3/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="assets/plugins/boostrapv3/css/bootstrap-theme.min.css" rel="stylesheet"
-        type="text/css" />
-    <link href="assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet"
-        type="text/css" />
-    <!-- END CORE CSS FRAMEWORK -->
-    <!-- BEGIN CSS TEMPLATE -->
-    <link href="assets/css/style.css" rel="stylesheet" type="text/css" />
-    <link href="assets/css/magic_space.css" rel="stylesheet" type="text/css" />
-    <link href="assets/css/responsive.css" rel="stylesheet" type="text/css" />
-    <link href="assets/css/animate.min.css" rel="stylesheet" type="text/css" />
-    <!-- END CSS TEMPLATE -->
-    <script type="text/javascript" src="assets/plugins/jquery-1.8.3.min.js"></script>
-    <!-- SLIDER REVOLUTION 4.x SCRIPTS  -->
-    <script type="text/javascript" src="assets/plugins/slider-plugin/js/slider1.min.js"></script>
-    <script type="text/javascript" src="assets/plugins/slider-plugin/js/slider2.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="assets/plugins/slider-plugin/css/settings.css"
-        media="screen" />
-</head>
-<!-- END HEAD -->
-<body>
+<?php
+// include("asset_php/sessionManager.php");
+include('asset_php/connect.php');
+
+$msg = "";
+    if(isset($_GET["error"])){
+        $error = $_GET["error"];
+        if($error=="undefined"){
+            $msg .= "Email/Password does not match!";
+        }
+        elseif($error=="mempty"){
+            $msg .= "invalid email";
+        }
+        elseif($error=="pblank"){
+            $msg .= "invalid password";
+        }
+        
+    
+    }
+
+$email = $password = "";
+$email_err = $password_err = "";
+
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Check if email is empty
+    if(empty(trim($_POST["email"]))){
+        $email_err = "Please enter email.";
+        header("location: login.php?error=mempty");
+    } else{
+        $email = trim($_POST["email"]);
+    }
+    
+    // Check if password is empty
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";
+        header("location: login.php?error=pblank");
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Validate credentials
+    if(empty($email_err) && empty($password_err)){
+        // Prepare a select statement
+        $sql = "SELECT id, name, email, password FROM client WHERE email = ?";
+        
+        if($stmt = mysqli_prepare($db, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_email);
+            
+            // Set parameters
+            $param_email = $email;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Store result
+                mysqli_stmt_store_result($stmt);
+                
+                // Check if email exists, if yes then verify password
+                if(mysqli_stmt_num_rows($stmt) == 1){                    
+                    // Bind result variables
+                    mysqli_stmt_bind_result($stmt, $id,$name, $email, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
+                            // Password is correct, so start a new session
+                            session_start();
+                            
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["email"] = $email;                            
+                            $_SESSION["name"] = $name;                            
+                            
+                            // Redirect user to welcome page
+                            header("location: dashboard.php");
+                        } else{
+                            // Display an error message if password is not valid
+                            header("location: login.php?error=undefined");
+                        }
+                    }
+                } else{
+                    // Display an error message if email doesn't exist
+                    header("location: login.php?error=undefined");
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+                header("location: login.php?error=undefined");
+            }
+        }
+        
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($db);
+}
+
+
+?>
+
 <div role="navigation" class="navbar navbar-default navbar-static-top">
             <div class="container">
                 <div class="compressed">
@@ -94,24 +159,24 @@
     <div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100">
-				<form class="login100-form validate-form">
+				<form class="login100-form validate-form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
 					<span class="login100-form-title p-b-26">
 						Welcome
 					</span>
-					<span class="login100-form-title p-b-48">
-						<i class="zmdi zmdi-font"></i>
-					</span>
+                    <div id = "warn" style="background-color:red;color:white;text-align:center;margin-bottom:20px">
+                        <?php if(!empty($msg)){ echo $msg; } ?>
+                    </div>
 
-					<div class="wrap-input100 validate-input" data-validate = "Valid email is: a@b.c">
+					<div class="wrap-input100">
 						<input class="input100" type="text" name="email">
 						<span class="focus-input100" data-placeholder="Email"></span>
 					</div>
 
-					<div class="wrap-input100 validate-input" data-validate="Enter password">
+					<div class="wrap-input100">
 						<span class="btn-show-pass">
 							<i class="zmdi zmdi-eye"></i>
 						</span>
-						<input class="input100" type="password" name="pass">
+						<input class="input100" type="password" name="password">
 						<span class="focus-input100" data-placeholder="Password"></span>
 					</div>
 
@@ -129,7 +194,7 @@
 							Don’t have an account?
 						</span>
 
-						<a class="txt2" href="#">
+						<a class="txt2" href="register.php">
 							Sign Up
 						</a>
 					</div>
